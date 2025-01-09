95823d893e9b0004097545355e0f1386d66663fb
black-adm <mattheuscontato17@gmail.com>
2025-01-09 17:14:03 -0300

    fix: static images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="min-h-screen w-full bg-slate-950 flex flex-col justify-center items-center space-y-4">
-      <h1 className="text-6xl text-white font-bold">
-        Generalcred
-      </h1>
-      <p className="max-w-lg w-full text-lg font-medium text-muted-foreground">
-        A General nasceu em 2021 com o objetivo de se tornar uma referência no mercado financeiro, abrindo 
-        novos caminhos para os seus clientes e fornecedores, por quem é sempre lembrada e recomendada.
-      </p>
+    <div className="min-h-screen w-full bg-gradient-to-br from-slate-950 to-slate-900 flex justify-center items-center space-y-4">
+      <div className="flex flex-col space-y-4">
+        <Image
+          src="/logo.png"
+          className="w-1/4"
+          alt="logo da Generalcred"
+          width={500}
+          height={500}
+        />
+
+        <p className="max-w-lg w-full text-lg font-medium text-muted-foreground">
+          A General nasceu em 2021 com o objetivo de se tornar uma referência no mercado financeiro, abrindo
+          novos caminhos para os seus clientes e fornecedores, por quem é sempre lembrada e recomendada.
+        </p>
+      </div>
     </div>
   );
 }
