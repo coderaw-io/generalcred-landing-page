f4b276a3c120b770ad6c1b3d8f79e0c92ea29286
black-adm <mattheuscontato17@gmail.com>
2025-01-10 14:48:33 -0300

    fix: update details

--- src/app/emprestimo/fgts/page.tsx
+++ src/app/emprestimo/fgts/page.tsx
@@ -1,24 +1,16 @@
+import Carousel from "@/components/shared/carousel"
 import { Footer } from "@/components/shared/footer"
 import { Header } from "@/components/shared/header"
-<<<<<<< HEAD
-import Carousel from "@/components/shared/carousel"
-import { PersonalDataForm } from "./form"
-=======
 import { PersonalDataForm } from "./_components/form"
->>>>>>> 806568b057158d029af20298d7d5396fce7b5a42
 
 export default function FgtsPage() {
   return (
     <div className="min-h-screen bg-gradient-to-r from-[#000814] via-[#051923] to-slate-950">
       <Header />
-<<<<<<< HEAD
-      <PersonalDataForm />
-      <Carousel />
-=======
       <main className="pt-12 md:pt-16">
         <PersonalDataForm />
       </main>
->>>>>>> 806568b057158d029af20298d7d5396fce7b5a42
+      <Carousel />
       <Footer />
     </div>
   )
