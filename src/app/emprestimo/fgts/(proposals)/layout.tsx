abf02c384bdc540cafad4dc2a7d0c2b8ca57a5d6
black-adm <mattheuscontato17@gmail.com>
2025-01-29 11:10:39 -0300

    fix: update fgts page and layout

---
+++ src/app/emprestimo/fgts/(proposals)/layout.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { Footer } from "@/components/shared/footer";
+import { Header } from "@/components/shared/header";
+import { useLoanProposals } from "@/hooks/use-loan-proposals-store";
+import { useRouter } from "next/navigation";
+import { useLayoutEffect } from "react";
+
+export default function FgtsLoanProposalsLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { formData, loanProposals } = useLoanProposals();
+  const router = useRouter();
+
+  useLayoutEffect(() => {
+    if (!formData || !loanProposals) {
+      router.push("/");
+    }
+  }, [formData, loanProposals, router])
+
+  return (
+    <div className="min-h-screen w-full">
+      <Header />
+      <div className="bg-slate-100 size-full overflow-hidden">
+        {children}
+      </div>
+      <Footer />
+    </div>
+  );
+}
