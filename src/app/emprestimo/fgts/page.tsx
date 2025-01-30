479873ad165bda4ce0132b4a334a05735a6858f9
black-adm <mattheuscontato17@gmail.com>
2025-01-30 15:59:14 -0300

    fix: update

--- src/app/emprestimo/fgts/page.tsx
+++ src/app/emprestimo/fgts/page.tsx
@@ -17,19 +17,24 @@ export default function FgtsPage() {
     <>
       <div className="min-h-screen overflow-hidden bg-gradient-to-r from-[#000814] via-[#051923] to-slate-950">
         <Header />
+
         <main className="w-full pt-12 md:pt-16">
           <PersonalDataForm setFormData={setFormData} setLoanProposals={setLoanProposals} />
         </main>
+
         <BenefitsSection />
+
         <div className="bg-slate-50 w-ful px-3 py-16 rounded-none sm:py-0 sm:pt-16 md:px-8 lg:px-12 xl:px-24 xl:py-24 2xl:px-28">
           <div className="w-full flex flex-col items-center space-y-12 lg:space-y-0 lg:flex-row lg:justify-between">
             <InformationSection />
             <CarouselSection />
           </div>
         </div>
+
         <FAQSection />
         <Footer />
       </div>
+      
       <WhatsappButton />
     </>
   )
